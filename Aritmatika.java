a20c1b5c3774dd29127b2d2854d9149424119ba9
jofanrizki <jofan_rizki_29rpl@student.smktelkom-mlg.sch.id>
2021-07-28 20:30:46 +0700

    bisaa

---
+++ Aritmatika.java
@@ -0,0 +1,15 @@
+
+package pbo;
+
+public class Aritmatika {
+    public static void main (String[] args) {
+        
+        double bil_a, bil_b, hasil;
+        bil_a = 454.55;
+        bil_b = 546.55;
+        hasil = bil_a + bil_b;
+        
+        System.out.println("Hasil Penjumlahan kedua Bilangan : ");
+        System.out.println(hasil);
+    }
+}
